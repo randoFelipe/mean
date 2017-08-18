d33be7006b835d467116ebbed5384ce2f65863a7
Felipe Rando <felipe.rando@rivendel.com.br>
2017-08-18 08:51:09 -0300

    express inicial

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  if (req.url === '/favicon.ico') {
-    return res.end();
-}
-console.log('Incoming request to ' + req.url);
-  var i = 2;
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  setTimeout(function() {
-    fs.readFile(__filename, {
-      encoding: 'utf8'
-    }, function (error, contents) {
-      if (error) {
-        console.error(error);
-        return res.end();
-      }
-      console.log('sending response for ' + req.url);
-      res.end(contents);
-    });
-  }, 5000);
-  while(i--) {
-    console.log('Loop value: '
-  }
-  }).listen(1337, '127.0.0.1');
-    + i + '\r');
-    console.log('Server running at http://127.0.0.1:1337/');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
